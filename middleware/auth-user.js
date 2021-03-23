4dbce010c8a048c1e143e2491b3df180a00d95b8
Ken Robinson <subscriptions@kodehaus.com>
2021-03-23 00:58:10 -0400

    Handle Failed User Authentication

--- middleware/auth-user.js
+++ middleware/auth-user.js
@@ -7,37 +7,46 @@ const { User } = require('../models');
 exports.authenticateUser = async (req,res, next) =>  {
   // Parse the user's credentials from the Authorization header.
   const credentials = auth(req);
+  let message;
   
   
   // If the user's credentials are available...
   if(credentials){
       // Attempt to retrieve the user from the data store
+      // by their username (i.e. the user's "key"
+      // from the Authorization header).
       const user = await User.findOne({ where: {username: credentials.name} });
+      // If a user was successfully retrieved from the data store...
+         // Use the bcrypt npm package to compare the user's password
+         // (from the Authorization header) to the user's password
+         // that was retrieved from the data store.
       if(user) {
           const authenticated = bcrypt.compareSync(credentials.pass, user.confirmedPassword);
+          // If the passwords match...
+             // Store the retrieved user object on the request object
+             // so any middleware functions that follow this middleware function
+             // will have access to the user's information.
           if(authenticated){
             console.log(`Authentication successful for username: ${user.username}`);
               req.currentUser = user;
+          } else {
+            message = `Authentication failure for username: ${user.username}`;
           }
-      }
+      } else {
+        message = `User not found for username: ${credentials.name}`;
+      };
+  } else {
+      message = 'Auth header not found';
   }
-// by their username (i.e. the user's "key"
-// from the Authorization header).
-
-  // If a user was successfully retrieved from the data store...
-     // Use the bcrypt npm package to compare the user's password
-     // (from the Authorization header) to the user's password
-     // that was retrieved from the data store.
-
-  // If the passwords match...
-     // Store the retrieved user object on the request object
-     // so any middleware functions that follow this middleware function
-     // will have access to the user's information.
 
   // If user authentication failed...
-     // Return a response with a 401 Unauthorized HTTP status code.
-
-  // Or if user authentication succeeded...
-     // Call the next() method.
+  // Return a response with a 401 Unauthorized HTTP status code.
+  if(message) {
+    console.warn(message);
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    // Or if user authentication succeeded...
+    // Call the next() method.
     next();
+  };
 };
